e66d76b47c96df4849866074cca09a9550678cc1
KayongoYongo <sknsammie10@gmail.com>
2023-08-08 17:14:18 +0000

    Basic auth update

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""A clas that impliments the Basic Auth
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    This class impliments the basic implimentation
+
+    Parameters:
+        It imports from Auth
+
+    Return:
+        None
+    """
+    pass
