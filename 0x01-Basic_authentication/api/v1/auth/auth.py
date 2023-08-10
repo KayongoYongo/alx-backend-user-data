df9c84ffd56e1395c785744823e6c9902e1f8579
KayongoYongo <sknsammie10@gmail.com>
2023-08-10 16:31:39 +0000

    Define routes which do not need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -25,8 +25,13 @@ class Auth:
         if excluded_paths is None or len(excluded_paths) == 0:
             return True
 
+        if path in excluded_paths:
+            return False
+
         for excluded_path in excluded_paths:
-            if excluded_path.endswith('*'):
+            if excluded_path.startswith(path):
+                return False
+            elif excluded_path.endswith('*'):
                 prefix = excluded_path[:-1]
                 if path.startswith(prefix):
                     return False
