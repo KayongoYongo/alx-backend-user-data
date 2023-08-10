0e3942bea37c16ce2401b2f000ba2f2a6062ce5f
KayongoYongo <sknsammie10@gmail.com>
2023-08-10 19:23:46 +0000

    create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,10 +2,31 @@
 """A class that impliments session auth
 """
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """
     This class deals with session Authentication
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        This function creates a session.
+
+        Parameters:
+            user_id: The parameter used to create a session
+
+        Return:
+            None or a string
+        """
+        if user_id is None:
+            return None
+
+        if not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
