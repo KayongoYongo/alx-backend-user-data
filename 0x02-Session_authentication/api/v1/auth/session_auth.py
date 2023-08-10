8fb166b998bbf4d03be4c65cebc3252029cfd175
KayongoYongo <sknsammie10@gmail.com>
2023-08-10 19:04:01 +0000

    Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""A class that impliments session auth
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    This class deals with session Authentication
+    """
+    pass
