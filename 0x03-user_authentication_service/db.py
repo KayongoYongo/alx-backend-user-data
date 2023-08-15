88b2d8a042b2e41b78eb2a798e0406be7def8fb2
KayongoYongo <sknsammie10@gmail.com>
2023-08-15 10:39:48 +0000

    db_add_user_update

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 """DB module
 """
+from user import User, Base
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
-from user import User, Base
 
 
 class DB:
@@ -45,6 +44,7 @@ class DB:
 
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
+        self._session.commit()
         return user
 
     def find_user_by(self, **kwargs) -> User:
@@ -70,8 +70,7 @@ class DB:
 
         if user is None:
             raise NoResultFound
-        else:
-            return user
+        return user
 
     def update_user(self, user_id: int, **kwargs) -> None:
         """
