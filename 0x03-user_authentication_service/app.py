1b9595032e220ec46da95f0d33386dc88077ad03
KayongoYongo <sknsammie10@gmail.com>
2023-08-16 14:56:41 +0000

    impliment login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -37,5 +37,30 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route('/sessions', methods=['POST'])
+def login() -> str:
+    """A function theat impliments a log in functionality
+
+    Args:
+        None
+
+    Return:
+        A JSONIFY message and return response
+    """
+    # Get the form data
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not (AUTH.valid_login(email, password)):
+        abort(401)
+    else:
+        # if the form data is correct
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+
+     return response
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
